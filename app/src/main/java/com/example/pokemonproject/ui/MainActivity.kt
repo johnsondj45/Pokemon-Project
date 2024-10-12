42862e3b6a004a9c91bbedc07c60295697b5ed8d
Dylan Johnson <dj2330@gmail.com>
2024-10-12 12:08:34 -0400

    * Created working branch * lint updates

--- app/src/main/java/com/example/pokemonproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokemonproject/ui/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.pokemonproject.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_layout)
     }
+
 }
