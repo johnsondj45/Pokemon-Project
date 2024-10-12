bc3d172f67503949285ca00a40c672828c561d70
Dylan Johnson <Dilly@MacBook-Pro-2.local>
2024-10-12 11:54:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/pokemonproject/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pokemonproject.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pokemonproject.R
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity_layout)
+    }
+}
