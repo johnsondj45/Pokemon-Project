7a318f5cfa27055099bbfbfae21030ef4439a9cc
Dylan Johnson <dj2330@gmail.com>
2024-10-12 11:58:58 -0400

    * Created Develop * Removed unused libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,11 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.navigation.ui)
     implementation(libs.androidx.navigation.fragment)
+
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/com/example/pokemonproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokemonproject/ui/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokemonproject.R
 
-class MainActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?){
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_layout)
     }
